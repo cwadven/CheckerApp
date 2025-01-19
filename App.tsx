b269819c2eab789d005d5670694097c086725386
이창우(Changwoo Lee) <changwoo.lee@wesang.com>
2025-01-20 08:58:39 +0900

    Add all

--- App.tsx
+++ App.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { RootNavigator } from "./src/navigation/RootNavigator";
 import { AuthProvider } from "./src/contexts/AuthContext";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
-    </AuthProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AuthProvider>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </AuthProvider>
+    </GestureHandlerRootView>
   );
 }
